a3a1407a9d2f98eca0601301f4c9681e25d9e4b3
Stan Wielga <svwielga4@gmail.com>
2019-12-30 11:08:52 -0800

    added routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,34 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('contacts', function() {
+  return Contact::latest()->orderBy('created_at', 'desc')->get();
+});
+
+Route::get('contact/{id}', function($id) {
+  return Contact::findOrFail($id);
+});
+
+Route::post('contact/store', function(Request $request) {
+  return Contact::create([
+    'name' => $request->input('name'),
+    'email' => $request->input('email'),
+    'phone' => $request->input('phone'),
+  ]);
+});
+
+Route::patch('contact/{id}', function(Request $request, $id) {
+  return Contact::findOrFail($id)->update([
+    'name' => $request->input('name'),
+    'email' => $request->input('email'),
+    'phone' => $request->input('phone'),
+  ]);
+});
+
+Route::delete('contact/{id}', function($id) {
+  return Contact::destroy($id);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
